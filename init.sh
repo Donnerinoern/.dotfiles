4be66924d76aaa68a8230d4de56b5b4b5159c111
Donnerinoern <72634505+Donnerinoern@users.noreply.github.com>
2022-04-03 23:59:16 +0200

    Electron-flags, sway and some folders for structure

--- init.sh
+++ init.sh
@@ -2,3 +2,11 @@
 cd ~
 ln -s ~/.dotfiles/.gitconfig
 ln -s ~/.dotfiles/.zshrc
+ln -s ~/.dotfiles/.bashrc
+ln -s ~/.dotfiles/.p10k.zsh
+
+cd ~/.config
+ln -s ~/.dotfiles/config/electron-flags.conf
+
+cd ~/.config/sway
+ln -s ~/.dotfiles/config/sway
